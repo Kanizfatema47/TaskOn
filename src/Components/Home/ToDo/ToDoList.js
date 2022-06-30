01bfcf4d601bb1659dae4423cbc3153f305fbe05
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-07-01 00:59:30 +0600

    successfully deleted a tasks

--- src/Components/Home/ToDo/ToDoList.js
+++ src/Components/Home/ToDo/ToDoList.js
@@ -9,9 +9,21 @@ const ToDoList = () => {
             .then(data => setTasks(data))
     }, [])
 
-    const handleCheck =()=>{
-        
-    }
+    const handleDelete = (id) => {
+        const proceed = window.confirm("Are you sure you want to delete?");
+        if (proceed) {
+          const url = `http://localhost:5000/task/${id}`;
+          fetch(url, {
+            method: "DELETE",
+          })
+            .then((res) => res.json())
+            .then((result) => {
+              const remaining = tasks.filter((task) => task._id !== id);
+              setTasks(remaining);
+            });
+        }
+      };
+  
     return (
         <div className='grid grid-cols-3 gap-10 justify-center'>
             {
@@ -19,11 +31,11 @@ const ToDoList = () => {
                 tasks?.map(task => (
                     <div class="form-control justify-center items-center flex flex-row">
                         <label class="cursor-pointer label">
-                            <input type="checkbox" onClick={handleCheck} class="checkbox checkbox-accent" />
+                            <input type="checkbox"  class="checkbox checkbox-accent" />
                         </label>
                         <p>{task.tasks}</p>
                         <button type="submit" className='btn btn-success mx-2'>Edit</button>
-                        <button type="submit" className='btn btn-danger mx-2'>Delete</button>
+                        <button type="submit"onClick={()=>handleDelete(task._id)}  className='btn btn-danger mx-2'>Delete</button>
 
                     </div>
                 ))
